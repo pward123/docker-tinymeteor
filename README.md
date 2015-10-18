4c4cdff2a11af7ee00648d432848b23d8428fd45
Diem Digital Support <pward@savicontrols.com>
2015-10-18 16:03:54 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+docker-tinymeteor
+=================
+
+A tiny docker image (~20mb) that can be used to run meteor apps. Inspired by [docker-tinynode](https://github.com/jprjr/docker-tinynode)
+
+This image has NodeJS 0.10.30 and NPM pre-installed
+
---
+++ tinymeteor_build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+imagename=tinymeteor
+
+cdir=$(pwd)
+source ./${imagename}_config
+
+buildroot_dir=${cdir}/buildroot-${buildroot_version}
+docker_dir=${cdir}/Docker
+
+if [[ ! -d ${buildroot_dir} ]]
+then
+  curl ${buildroot_url} | tar jx
+fi
+
+cp ./${imagename}_defconfig ${buildroot_dir}/configs/${imagename}_defconfig
+
+cd ${buildroot_dir} && make ${imagename}_defconfig && make
+for package in "${packages[@]}"
+do
+    echo "building ${package}"
+    make ${package}-rebuild
+done
+make
+
+mv ${buildroot_dir}/output/images/rootfs.tar ${docker_dir}
